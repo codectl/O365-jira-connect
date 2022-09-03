c44e1c35f4c7936fc3b9574b58bb363d19848477
renatodamas <rena2damas@gmail.com>
2022-09-03 17:19:02 +0100

    feat: configure db engine

---
+++ src/O365_jira_connect/components/__init__.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+Session = sessionmaker()
+
+
+def init_engine(engine_url, debug=False):
+    engine = create_engine(url=engine_url, echo=debug)
+    Session.configure(bind=engine)
+    Base.metadata.create_all(bind=engine)  # create tables
+
+
+def with_session(f):
+    """Create session for given function."""
+    def wrapper(*args, **kwargs):
+        session = Session()
+        session.begin()
+        f(*args, session=session, **kwargs)
+        session.close()
+    return wrapper
