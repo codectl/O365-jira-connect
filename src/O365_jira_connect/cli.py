8274c4b165f1c86ca92c1062e85e1ede44038ab6
renatodamas <rena2damas@gmail.com>
2022-09-03 22:30:51 +0100

    feat: add missing options and commands #wip

--- src/O365_jira_connect/cli.py
+++ src/O365_jira_connect/cli.py
@@ -7,16 +7,29 @@ from O365 import (
     MSGraphProtocol,
     MSOffice365Protocol,
 )
+from O365_notifications.streaming import O365StreamingSubscriber
+from O365_notifications.constants import O365EventType
 
+from O365_jira_connect.backend import DatabaseTokenBackend
 from O365_jira_connect.components import init_engine
-from O365_jira_connect.components.token import DatabaseTokenBackend
+from O365_jira_connect.filters import (
+    JiraCommentNotificationFilter,
+    RecipientControlFilter,
+    SenderEmailBlacklistFilter,
+    SenderEmailDomainWhitelistedFilter,
+    ValidateMetadataFilter,
+)
+from O365_jira_connect.handlers.jira import JiraNotificationHandler
 
+# configure logging
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # load environment if exists
 dotenv.load_dotenv(".env")
 
 
+@click.option("--debug/--no-debug", default=False, help="Enable debug")
 @click.option(
     "--database",
     "-d",
@@ -26,7 +39,6 @@ dotenv.load_dotenv(".env")
     show_envvar=True,
     help="the database URI used to store information on issues",
 )
-@click.option("--debug/--no-debug", default=False, help="Enable debug")
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
 def cli(debug, database):
     if debug:
@@ -35,101 +47,121 @@ def cli(debug, database):
     init_engine(engine_url=database, debug=debug)
 
 
+def o365_options(f):
+    f = click.option(
+        "--protocol",
+        required=False,
+        type=click.Choice(["graph", "office"]),
+        default="graph",
+        show_default=True,
+        help="the O365 protocol",
+    )(f)
+    f = click.option(
+        "--api-version",
+        required=False,
+        type=str,
+        help="the O365 API version",
+    )(f)
+    f = click.option(
+        "--principal",
+        required=True,
+        type=str,
+        help="the resource principal",
+        envvar="O365_PRINCIPAL",
+        show_envvar=True,
+    )(f)
+    f = click.option(
+        "--tenant-id",
+        required=True,
+        type=str,
+        help="the O365 tenant",
+        envvar="O365_TENANT_ID",
+        show_envvar=True,
+    )(f)
+    f = click.option(
+        "--client-id",
+        required=True,
+        type=str,
+        envvar="O365_CLIENT_ID",
+        show_envvar=True,
+        help="the O365 application/client id",
+    )(f)
+    f = click.option(
+        "--client-secret",
+        required=False,
+        type=str,
+        default=None,
+        envvar="O365_CLIENT_SECRET",
+        show_envvar=True,
+        help="the O365 client secret",
+    )(f)
+    f = click.option(
+        "--grant-type",
+        required=False,
+        type=click.Choice(["credentials", "authorization"]),
+        default="credentials",
+        show_default=True,
+        help="the OAuth2 grant type",
+    )(f)
+    f = click.option(
+        "--scopes",
+        required=False,
+        type=str,
+        multiple=True,
+        default=[],
+        envvar="O365_SCOPES",
+        show_envvar=True,
+        help="the O365 scopes",
+    )(f)
+    f = click.option(
+        "--retries",
+        required=False,
+        type=int,
+        default=0,
+        help="number of retries when request fails",
+    )(f)
+    return f
+
+
 @cli.command()
+@o365_options
+def authorize(**params):
+    """Grant service authorization to O365 resources."""
+    return authorize_account(**params)
+
+
 @click.option(
-    "--protocol",
-    required=False,
-    type=click.Choice(["graph", "office"]),
-    default="graph",
-    show_default=True,
-    help="the O365 protocol",
-)
-@click.option(
-    "--api-version",
-    required=False,
-    type=str,
-    help="the O365 API version",
-)
-@click.option(
-    "--principal",
-    required=True,
-    type=str,
-    help="the resource principal",
-    envvar="O365_PRINCIPAL",
-    show_envvar=True,
-)
-@click.option(
-    "--tenant-id",
-    required=True,
-    type=str,
-    help="the O365 tenant",
-    envvar="O365_TENANT_ID",
-    show_envvar=True,
-)
-@click.option(
-    "--client-id",
-    required=True,
-    type=str,
-    envvar="O365_CLIENT_ID",
-    show_envvar=True,
-    help="the O365 application/client id",
-)
-@click.option(
-    "--client-secret",
-    required=False,
-    type=str,
-    default=None,
-    envvar="O365_CLIENT_SECRET",
-    show_envvar=True,
-    help="the O365 client secret",
-)
-@click.option(
-    "--grant-type",
-    required=False,
-    type=click.Choice(["credentials", "authorization"]),
-    default="credentials",
-    show_default=True,
-    help="the OAuth2 grant type",
-)
-@click.option(
-    "--scopes",
-    required=False,
-    type=str,
-    multiple=True,
-    default=[],
-    envvar="O365_SCOPES",
-    show_envvar=True,
-    help="the O365 scopes",
-)
-@click.option(
-    "--retries",
+    "--keep-alive-interval",
     required=False,
     type=int,
-    default=0,
-    help="number of retries when request fails",
+    envvar="KEEP_ALIVE_INTERVAL_IN_SECONDS",
+    show_envvar=True,
+    help="the interval of seconds a keep-alive message is sent",
 )
-def authorize(
-    protocol,
-    api_version,
-    principal,
-    tenant_id,
-    client_id,
-    client_secret,
-    grant_type,
-    scopes,
-    retries,
-):
-    """Grant service authorization to O365 resources."""
-    return authorize_account(
-        protocol=protocol,
-        api_version=api_version,
-        principal=principal,
-        tenant_id=tenant_id,
-        client_id=client_id,
-        client_secret=client_secret,
-        grant_type=grant_type,
-        scopes=scopes,
-        retries=retries,
+@click.option(
+    "--connection-timeout",
+    required=False,
+    type=int,
+    envvar="CONNECTION_TIMEOUT_IN_MINUTES",
+    show_envvar=True,
+    help="the O365 connection timeout in minutes",
+)
+@o365_options
+@cli.command()
+def handle_incoming_events(**params):
+    """Handle incoming O365 events."""
+    connection_timeout = params.pop("connection_timeout")
+    keep_alive_interval = params.pop("keep_alive_interval")
+
+    subscriber = create_subscriber(**params)
+    handler = create_handler(subscriber)
+
+    # start listening for streaming events ...
+    subscriber.start_streaming(
+        notification_handler=handler,
+        connection_timeout=connection_timeout,
+        keep_alive_interval=keep_alive_interval,
+        refresh_after_expire=True,
     )
 
 
@@ -174,3 +206,34 @@ def authorize_account(
         account.authenticate(tenant_id=tenant_id)
         logger.info("Authorization done.")
     return account
+
+
+def create_subscriber(principal: str = None, **kwargs):
+    account = authorize_account(principal=principal, **kwargs)
+    mailbox = account.mailbox()
+
+    # create a new streaming subscriber
+    subscriber = O365StreamingSubscriber(parent=account)
+
+    # subscribe to inbox and sent items folder events
+    events = [O365EventType.CREATED]
+    subscriber.subscribe(resource=mailbox.inbox_folder(), events=events)
+    subscriber.subscribe(resource=mailbox.sent_folder(), events=events)
+    return subscriber
+
+
+def create_handler(subscriber, **kwargs):
+    whitelist = kwargs["EMAIL_WHITELISTED_DOMAINS"]
+    blacklist = kwargs["EMAIL_BLACKLIST"]
+    resources = [sub.resource for sub in subscriber.subscriptions]
+    main_resource = subscriber.main_resource
+    filters = [
+        JiraCommentNotificationFilter(folder=resources[0]),
+        RecipientControlFilter(email=main_resource, ignore=[resources[1]]),
+        SenderEmailBlacklistFilter(blacklist=blacklist),
+        SenderEmailDomainWhitelistedFilter(whitelisted_domains=whitelist),
+        ValidateMetadataFilter(),
+    ]
+    return JiraNotificationHandler(
+        parent=subscriber, namespace=subscriber.namespace, filters=filters
+    )
