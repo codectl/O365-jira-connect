8c35ce834da0a8845a744759f57015f7ff340853
renatodamas <rena2damas@gmail.com>
2022-09-03 17:53:27 +0100

    style: remove blank space

--- src/O365_jira_connect/components/__init__.py
+++ src/O365_jira_connect/components/__init__.py
@@ -3,14 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-
 Session = sessionmaker()
 
 
 def init_engine(engine_url, debug=False):
     engine = create_engine(url=engine_url, echo=debug)
     Session.configure(bind=engine)
-    Base.metadata.create_all(bind=engine)  # create tables
+    Base.metadata.create_all(bind=engine)
 
 
 def with_session(f):
