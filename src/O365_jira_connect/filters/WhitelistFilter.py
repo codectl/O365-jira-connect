f9fcc76f95751d874c30b65bc1d3716cbdfc608a
renatodamas <rena2damas@gmail.com>
2022-09-05 08:42:42 +0100

    refactor: rename constructor argument #minor

--- src/O365_jira_connect/filters/WhitelistFilter.py
+++ src/O365_jira_connect/filters/WhitelistFilter.py
@@ -11,15 +11,15 @@ class WhitelistFilter(OutlookMessageFilter):
     """Filter for message whitelist. It currently checks if the sender's domain is
     whitelisted."""
 
-    def __init__(self, whitelisted):
-        self.whitelisted = whitelisted
+    def __init__(self, whitelist):
+        self.whitelist = whitelist
 
     def apply(self, message):
         if not message:
             return None
 
         sender = message.sender.address
-        if sender.split("@")[1] not in self.whitelisted:
+        if sender.split("@")[1] not in self.whitelist:
             logger.info(
                 f"Message skipped as the sender's email '{sender}' is not whitelisted."
             )
