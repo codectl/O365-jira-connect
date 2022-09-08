f4a470f73e7d26034eaf0a0d177ad48cdc18e8a4
renatodamas <rena2damas@gmail.com>
2022-09-08 11:55:09 +0100

    test: additional tests #wip

--- tests/unit/test_issue.py
+++ tests/unit/test_issue.py
@@ -9,10 +9,43 @@ def issue_s():
 
 
 class TestIssueSvc:
-    def test_create_message_body(self, issue_s):
-        body = issue_s.create_message_body(template="jira", values={
-            "author": "me@example.com",
-            "cc": "him@example.com",
-            "body": "some short body"}
-                                           )
-        print(body)
+    def test_create_message_body_with_jira_template(self, issue_s):
+        body = issue_s.create_message_body(
+            template="jira",
+            values={
+                "author": "me@example.com",
+                "cc": "him@example.com",
+                "body": "some short message body",
+            },
+        )
+        assert "From: me@example.com" in body
+        assert "Cc: him@example.com" in body
+        assert "some short message body" in body
+
+    def test_create_message_body_with_notification_template(self, issue_s):
+        url = "issuetracker/issues/UT-123"
+        body = issue_s.create_message_body(
+            template="notification",
+            values={
+                "username": "User",
+                "summary": "some generic issue summary",
+                "key": "UT-123",
+                "url": url,
+            },
+        )
+        assert "Dear User" in body
+        assert f"the issue [UT-123]({url}) was created." in body
+        assert f"track the progress of the issue [here]({url}) was created." in body
+
+    def test_create_message_body_with_reply_template(self, issue_s):
+        body = issue_s.create_message_body(
+            template="reply",
+            values={
+                "style": "",
+                "body": "some generic body issue",
+                "author": "User",
+                "reply": "this is a message reply",
+            },
+        )
+        assert "some generic body issue" in body
+        assert "this is a message reply" in body
