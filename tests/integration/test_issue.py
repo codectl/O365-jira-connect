ce1e64d9ac69b59d7b65dbe02c500308e9c8783b
renatodamas <rena2damas@gmail.com>
2022-09-09 09:47:16 +0100

    test: add guest_user fixture #minor

--- tests/integration/test_issue.py
+++ tests/integration/test_issue.py
@@ -13,4 +13,10 @@ def issue(jira_s, project, issue_type):
 
 
 class TestIssueSvc:
-    pass
+    @pytest.fixture(scope="class")
+    def guest_user(self, jira_s):
+        jira_s.add_user(username="guest", email="guest@example.com")
+        return next(jira_s.search_users(query="guest"))
+
+    def test_crud(self, issue_s, guest_user):
+        issue = issue_s.create(reporter=guest_user)
