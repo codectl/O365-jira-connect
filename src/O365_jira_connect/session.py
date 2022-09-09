6c7069a90a646268f8ec7124b9e8ee971eeaa7ed
renatodamas <rena2damas@gmail.com>
2022-09-09 09:41:22 +0100

    refactor: rename components to session #minor

--- src/O365_jira_connect/session.py
+++ src/O365_jira_connect/session.py
@@ -2,17 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from O365_jira_connect.services.issue import IssueSvc
-from O365_jira_connect.services.jira import JiraSvc
-
-__all__ = ("Base", "Session", "init_engine", "issue_s", "jira_s", "with_session")
+__all__ = ("Base", "Session", "init_engine", "with_session")
 
 Base = declarative_base()
 Session = sessionmaker()
-
-# initialize internal service components
-jira_s = JiraSvc()
-issue_s = IssueSvc(jira=jira_s)
 
 
 def init_engine(engine_url, debug=False):
